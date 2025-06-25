a12a10d3bf9777e8b697ecd5d4a74d94504ef95f
denis <139350389+finsterfurz@users.noreply.github.com>
2025-06-25 08:06:44 +0200

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -10,7 +10,7 @@ import {
   Key, 
   Users, 
   ChevronDown 
-} from '../components/icons';
+} from '../components/icons/AllIcons';
 
 // ==================== HOMEPAGE ====================
 const Homepage = () => {
